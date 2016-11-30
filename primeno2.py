9512a2a29ed1b5e34c50e4504d366fd06be32e33
Millicent <Millicent>
2016-11-30 19:36:17 +0300

    adding code

--- primeno2.py
+++ primeno2.py
@@ -1,5 +1,19 @@
 #Function that find numbers between 0-n
 def prime_num(number):
     #The list of prime numbers
-   pass
+    primenumbers=[]
+    isdivisible=True
+    if number<0:
+        print "There are no negative prime numbers"
+    else:
+        for i in range(2,number+1):
+            isdivisible=True
+            for j in range(2,i):
+                if i%j==0:
+                    isdivisible=False
+            if isdivisible==True:
+                primenumbers.append(i)
+    return primenumbers
+print prime_num(50)
+print prime_num(-5)
 
